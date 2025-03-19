e123d02d9575a258e316b1f9f919b2e24c1b4c26
MirzaCisco <53210148+MirzaCisco@users.noreply.github.com>
2025-03-19 14:05:03 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+# variables.tf
+
+variable "subscription_id" {
+  description = "The Azure subscription ID."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Azure Service Principal client ID."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Azure Service Principal client secret."
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The Azure tenant ID."
+  type        = string
+}
