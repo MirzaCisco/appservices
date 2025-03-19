68d7588781373f3e060bcf3f753715b5235362a7
MirzaCisco <53210148+MirzaCisco@users.noreply.github.com>
2025-03-19 14:07:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,35 +7,48 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  tenant_id = var.tenant_id
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
 }
 
-data "azuread_user" "emina" {
-  user_principal_name = "emina@mirzadevopsoutlook935.onmicrosoft.com"
-}
-
+# Create a Resource Group in Central US (change as needed)
 resource "azurerm_resource_group" "rg" {
   name     = "RG-RedSpoke-Central"
   location = "Central US"
 }
 
+# Create an App Service Plan (Linux)
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "example-serviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "S2"
+  sku_name            = "S1"
   os_type             = "Linux"
 }
 
+# Create an Azure Linux Web App
 resource "azurerm_linux_web_app" "app_service" {
   name                = "example-linuxwebapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
+  # Minimal required site_config block
   site_config {}
+
+  # Example app settings
+  app_settings = {
+    "PYTHON_VERSION" = "3.8"
+  }
 }
 
+# Retrieve an Azure AD user by their UPN
+data "azuread_user" "emina" {
+  user_principal_name = "emina@mirzadevopsoutlook935.onmicrosoft.com"
+}
+
+# Assign a Reader role to the user at the App Service scope
 resource "azurerm_role_assignment" "app_service_iam" {
   scope                = azurerm_linux_web_app.app_service.id
   role_definition_name = "Reader"
