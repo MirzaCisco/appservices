b6051d79857fbae2b380bb7a24c7f55af9770408
MirzaCisco <53210148+MirzaCisco@users.noreply.github.com>
2025-03-19 13:42:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,29 +23,29 @@ variable "tenant_id" {
   description = "The Azure tenant ID."
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "RG-RedSpoke"
-  location = "East US"
+# Reference the existing Resource Group "RG-RedSpoke"
+data "azurerm_resource_group" "rg" {
+  name = "RG-RedSpoke"
 }
 
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "example-serviceplan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku_name            = "S1"
   os_type             = "Linux"
 }
 
 resource "azurerm_linux_web_app" "app_service" {
   name                = "example-linuxwebapp"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
-  # A minimal site_config block is required.
+  # Minimal required site_config block.
   site_config {}
 
-  # Use app_settings to signal your runtime preference.
+  # Example app settings to indicate the runtime version.
   app_settings = {
     "PYTHON_VERSION" = "3.8"
   }
