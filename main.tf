36bd8586339b210d13b25f89e0909157936c2537
MirzaCisco <53210148+MirzaCisco@users.noreply.github.com>
2025-03-19 14:01:43 +0100

    Update main.tf
    Adding emina as IAM user to app service

--- main.tf
+++ main.tf
@@ -6,24 +6,14 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-variable "subscription_id" {
-  description = "The Azure subscription ID."
+provider "azuread" {
+  tenant_id = var.tenant_id
 }
 
-variable "client_id" {
-  description = "The Azure Service Principal client ID."
+data "azuread_user" "emina" {
+  user_principal_name = "emina@mirzadevopsoutlook935.onmicrosoft.com"
 }
 
-variable "client_secret" {
-  description = "The Azure Service Principal client secret."
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "The Azure tenant ID."
-}
-
-# Create a new Resource Group in Central US
 resource "azurerm_resource_group" "rg" {
   name     = "RG-RedSpoke-Central"
   location = "Central US"
@@ -43,11 +33,15 @@ resource "azurerm_linux_web_app" "app_service" {
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
-  # Minimal required site_config block.
   site_config {}
+}
 
-  # App settings to signal your runtime preference.
-  app_settings = {
-    "PYTHON_VERSION" = "3.8"
-  }
+resource "azurerm_role_assignment" "app_service_iam" {
+  scope                = azurerm_linux_web_app.app_service.id
+  role_definition_name = "Reader"
+  principal_id         = data.azuread_user.emina.object_id
+
+  depends_on = [
+    azurerm_linux_web_app.app_service
+  ]
 }
