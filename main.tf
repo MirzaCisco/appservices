e7ba432e0570e3bad2e898ec8f417c99e84570ad
MirzaCisco <53210148+MirzaCisco@users.noreply.github.com>
2025-03-19 13:36:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# Variable declarations for the Azure credentials
 variable "subscription_id" {
   description = "The Azure subscription ID."
 }
@@ -24,13 +23,11 @@ variable "tenant_id" {
   description = "The Azure tenant ID."
 }
 
-# Create the Resource Group RG-RedSpoke in East US
 resource "azurerm_resource_group" "rg" {
   name     = "RG-RedSpoke"
   location = "East US"
 }
 
-# Create a Service Plan using the new azurerm_service_plan resource
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "example-serviceplan"
   location            = azurerm_resource_group.rg.location
@@ -39,14 +36,17 @@ resource "azurerm_service_plan" "app_service_plan" {
   os_type             = "Linux"
 }
 
-# Create an Azure Linux Web App using the new azurerm_linux_web_app resource
 resource "azurerm_linux_web_app" "app_service" {
   name                = "example-linuxwebapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
-  application_stack {
-    python_version = "3.8"
+  # A minimal site_config block is required.
+  site_config {}
+
+  # Use app_settings to signal your runtime preference.
+  app_settings = {
+    "PYTHON_VERSION" = "3.8"
   }
 }
